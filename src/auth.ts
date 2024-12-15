8a3b156770da18faa0c82cbe8a8a0f420b9fc504
Dimitar Dimitrov <idimitak@gmail.com>
2024-12-15 17:38:20 +0100

    0.0.2 - Fixed Edge runtime Prisma

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,15 @@
-import { prisma } from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 import NextAuth from "next-auth";
 import github from "next-auth/providers/github";
+import { PrismaClient } from "@prisma/client";
+
+const neon = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+const adapter = new PrismaNeon(neon);
+const prisma = new PrismaClient({ adapter });
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
